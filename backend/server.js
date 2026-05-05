16db2f27dfbdecfb0f635a7d2433903fb3129ad9
9A-Ayush <wemayush@gmail.com>
2026-05-05 12:13:37 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ app.use('/api/users', require('./routes/users'));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(process.env.PORT, () => 
-      console.log(`🚀 Server running on port ${process.env.PORT}`)
-    );
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
+    )
   })
   .catch(err => console.log(err));
